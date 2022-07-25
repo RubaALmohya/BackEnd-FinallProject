eb59703f5cb6888c7025a1f523dd3a08413c6fca
RubaALmohya <ruba12399@gmail.com>
2022-07-25 13:16:20 +0300

    update changes

--- backend/mood_changer/mood_changer/urls.py
+++ backend/mood_changer/mood_changer/urls.py
@@ -5,6 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user_app/', include('user_app.urls')),
-    path('mood_app', include('mood_app.urls')),
-    path('profile_app', include('profile_app.urls')),
+    path('mood_app/', include('mood_app.urls')),
+    path('profile_app/', include('profile_app.urls')),
 ]
